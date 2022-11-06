e16d7a09f4f64fde76770fd71e056af58c2fd48f
Daniel Rocha <dgustavo@id.uff.br>
2022-11-06 16:25:58 -0300

    refactor: add nativebase configuration

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { Center, NativeBaseProvider, Text } from 'native-base';
 
 export default function App() {
   return (
     <NativeBaseProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Center flex={1} bgColor="black">
+        <Text color="white" fontSize={24}>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
-      </View>
+      </Center>
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
