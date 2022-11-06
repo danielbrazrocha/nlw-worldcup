c9ca0ce28354b550f849734ad9fe4d2239f9db7e
Daniel Rocha <dgustavo@id.uff.br>
2022-11-06 14:44:48 -0300

    feat: render first endpoint return

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,20 @@
+interface HomeProps {
+  count: number
+}
 
-export default function Home() {
+export default function Home(props: HomeProps) {
   return (
-    <h1>Hi everyone!</h1>
+    <h1>{props.count}</h1>
     )
+}
+
+export const getServerSideProps = async () => {
+  const res = await fetch('http://localhost:3333/pools/count') 
+  const data = await res.json()
+
+  return {
+    props: {
+      count: data.count,
+    }
+  }
 }
