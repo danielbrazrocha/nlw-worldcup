e0e34e46f02e0b63d2a00b1c191b2ca73e7a9f4b
Daniel Rocha <dgustavo@id.uff.br>
2022-11-06 17:29:41 -0300

    fix: change component names

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar, NativeBaseProvider} from 'native-base';
-import { THEME } from './src/styles/theme'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { THEME } from './src/styles/theme'
 
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
