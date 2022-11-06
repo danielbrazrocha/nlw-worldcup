9d942380ad2834cda6428501971d962333130dd2
Daniel Rocha <dgustavo@id.uff.br>
2022-11-06 16:21:24 -0300

    feat: add nativebase provider

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
