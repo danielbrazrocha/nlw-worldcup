bb525d301de28b75ade8e5c9aea5f75ccf94c582
Daniel Rocha <dgustavo@id.uff.br>
2022-11-06 12:18:16 -0300

    feat: add route configuration

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,15 @@ import Fastify from 'fastify';
 async function start() {
   const fastify = Fastify({ logger: true });
 
+  fastify.get('/pools/count', () => {
+    return {count: 0}
+  })
+
+
+
   await fastify.listen({ port: 3333 });
+
+
 }
 
 start()
