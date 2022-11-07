496acaf296aa7d24b9826997b6141cea848cb56f
Daniel Rocha <dgustavo@id.uff.br>
2022-11-07 13:41:00 -0300

    feat: add route and jwt configuration

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,12 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { Pools } from './src/screens/Pools';
 
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
 import { AuthContextProvider } from './src/contexts/AuthContext';
-import { SignIn } from './src/screens/SignIn';
-import { Find } from './src/screens/Find';
-import { New } from './src/screens/New';
+import { Routes } from './src/routes';
 
 export default function App() {
 
@@ -23,7 +20,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
           />
-        {fontsLoaded ? <SignIn /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
--- mobile/src/components/Header.tsx
+++ mobile/src/components/Header.tsx
@@ -3,6 +3,8 @@ import { CaretLeft, Export } from 'phosphor-react-native';
 
 import { ButtonIcon } from './ButtonIcon';
 
+import { useNavigation } from '@react-navigation/native';
+
 interface Props {
   title: string;
   showBackButton?: boolean;
@@ -12,12 +14,14 @@ interface Props {
 export function Header({ title, showBackButton = false, showShareButton = false }: Props) {
   const EmptyBoxSpace = () => (<Box w={6} h={6} />);
 
+  const { navigate } = useNavigation();
+
   return (
     <HStack w="full" h={24} bgColor="gray.800" alignItems="flex-end" pb={5} px={5}>
       <HStack w="full" alignItems="center" justifyContent="space-between">
         {
           showBackButton
-            ? <ButtonIcon icon={CaretLeft} />
+            ? <ButtonIcon icon={CaretLeft} onPress={() => navigate('pools')} />
             : <EmptyBoxSpace />
         }
 
