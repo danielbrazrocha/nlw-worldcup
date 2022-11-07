23e40d8b514048206b11fd273da7c4b90e6bfb6c
Daniel Rocha <dgustavo@id.uff.br>
2022-11-06 22:26:07 -0300

    feat: tipando SVG

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,9 +1,10 @@
 import { Center, Text } from 'native-base';
+import Logo from '../assets/logo.svg'
 
 export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900">
-      <Text color="white" fontSize={24}>SIGNIN</Text>
+      <Logo />
     </Center>
   );
 }
